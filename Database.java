42d464fb70585371f6fe39d3d6de396fccaf1f0c
nbeel khalaf <nbeel_mofeed@hotmail.com>
2015-02-08 11:03:42 +0200

    nbeel
    here is my worke

---
+++ Database.java
@@ -0,0 +1,9 @@
+package postG.test;
+
+public interface Database {
+	String url = "jdbc:postgresql://localhost:5432/";
+	String dbName = "testN";
+	String driver = "org.postgresql.Driver";
+	String userName = "postgres";
+	String password = "";
+}
